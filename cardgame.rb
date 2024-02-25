9402f27fa11e071ce1555b68948b3633fac03a43
matsutakechan2go <matsutakechan_nigo@aol.com>
2024-02-25 19:18:58 +0900

    メソッド名だけつけておく

--- cardgame.rb
+++ cardgame.rb
@@ -1,10 +1,31 @@
 
 #定数の定義
-MARK = ['ハート', 'ダイア', 'クローバー', 'スペード']
-NUMBER = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'ジャック', 'クイーン', 'キング', 'エース']
+MARKS = ['ハート', 'ダイア', 'クローバー', 'スペード']
+NUMBERS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'ジャック', 'クイーン', 'キング', 'エース']
 
-def card_game
+#カードを生成するメソッド
+def create_card
+  card = [] #カードを格納するための箱をつくる
+  MARKS.each do |mark|
+    NUMBERS.each do |number|
+      card << { mark: mark, number: number}
+    end
+  end
+    card #上記で作成されたカードを呼び出す
+end
+
+#カードをシャッフルするメソッド
+def shuffle(card)
+  card.shuffle!
+end
+
+#カードを配るメソッド
+def hands_card(card)
 
 end
+
+
+#ゲームを開始するメソッド
+def card_game
 
 card_game
